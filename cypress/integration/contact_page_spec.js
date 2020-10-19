c039a34f33bdf8b86a9a55473de37abdc76dd5ec
BasnetShradha <basnetshradha10@gmail.com>
2020-10-19 19:45:55 +1100

    Add case when all the fields have valid data.

--- cypress/integration/contact_page_spec.js
+++ cypress/integration/contact_page_spec.js
@@ -12,4 +12,30 @@ describe('Contact Form', () => {
       cy.contains('Did you forget to leave us a message?')
     })
   })
+
+  context('when all the fields have valid data', () => {
+    it('returns success messages', () => {
+      cy.visit(url)
+
+      cy.get('[data-cy=input-name]')
+        .click()
+        .type('Bella Vista')
+
+      cy.get('[data-cy=input-email]')
+        .click()
+        .type('bella.vista@test.com')
+
+      cy.get('[data-cy=input-phone]')
+        .click()
+        .type('1234456789')
+
+      cy.get('[data-cy=input-message]')
+        .click()
+        .type('Hi, This is Bella Vista.')
+
+      cy.contains('Submit').click()
+
+      cy.contains('Your message has been sent :)')
+    })
+  })
 })
