3a647f8ae9bea9fed70a06d796a018189987725f
BasnetShradha <basnetshradha10@gmail.com>
2020-10-19 20:25:58 +1100

    Add cases for contact name field in the contact form.

--- cypress/integration/contact_page_spec.js
+++ cypress/integration/contact_page_spec.js
@@ -1,5 +1,6 @@
 describe('Contact Form', () => {
   let url = 'http://sample-website.beforeyoubid.com.au/contact'
+  let long_string = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
   context('when none of the fields are filled', () => {
     it('returns validation error messages', () => {
@@ -14,6 +15,80 @@ describe('Contact Form', () => {
   })
 
   context('when some of the fields have various forms of data', () => {
+    context('contact name', () => {
+      context('when contact name has special characters', () => {
+        it('does not return validation error message', () => {
+          cy.visit(url)
+
+          cy.get('[data-cy=input-name]')
+            .click()
+            .type('Bella %%% Vista')
+
+          cy.get('[data-cy=input-email]')
+            .click()
+            .type('bella.vista@test.com')
+
+          cy.get('[data-cy=input-phone]')
+            .click()
+            .type('1234456789')
+
+          cy.get('[data-cy=input-message]')
+            .click()
+            .type('Hi, This is Bella Vista.')
+
+          cy.contains('Submit').click()
+          cy.contains('Your message has been sent :)')
+        })
+      })
+
+      context('when contact name are numbers', () => {
+        it('does not return validation error message', () => {
+          cy.visit(url)
+          cy.get('[data-cy=input-name]')
+            .click()
+            .type('123456')
+
+          cy.get('[data-cy=input-email]')
+            .click()
+            .type('bella.vista@test.com')
+
+          cy.get('[data-cy=input-phone]')
+            .click()
+            .type('1234456789')
+
+          cy.get('[data-cy=input-message]')
+            .click()
+            .type('Hi, This is Bella Vista.')
+
+          cy.contains('Submit').click()
+          cy.contains('Your message has been sent :)')
+        })
+      })
+
+      context('when contact name has long string', () => {
+        it('does not return validation error message', () => {
+          cy.visit(url)
+          cy.get('[data-cy=input-name]')
+            .click()
+            .type(long_string)
+
+          cy.get('[data-cy=input-email]')
+            .click()
+            .type('bella.vista@test.com')
+
+          cy.get('[data-cy=input-phone]')
+            .click()
+            .type('1234456789')
+
+          cy.get('[data-cy=input-message]')
+            .click()
+            .type('Hi, This is Bella Vista.')
+
+          cy.contains('Submit').click()
+          cy.contains('Your message has been sent :)')
+        })
+      })
+    })
   })
 
   context('when all the fields have valid data', () => {
