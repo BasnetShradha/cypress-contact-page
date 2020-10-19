ad752f7014580e907277f10fa05fe44fb91ce585
BasnetShradha <basnetshradha10@gmail.com>
2020-10-19 19:10:35 +1100

    Add spec file for contact form.

---
+++ cypress/integration/contact_page_spec.js
@@ -0,0 +1,7 @@
+describe('Contact Form', () => {
+  context('when page loads', () => {
+    it('sample test', () => {
+      cy.visit('http://sample-website.beforeyoubid.com.au/contact')
+    })
+  })
+})
