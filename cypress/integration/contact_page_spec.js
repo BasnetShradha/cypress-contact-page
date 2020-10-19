8b14ce39c102d1569b369145148223ff2bd021ed
BasnetShradha <basnetshradha10@gmail.com>
2020-10-19 19:34:17 +1100

    Add case when none of the fields are filled.

--- cypress/integration/contact_page_spec.js
+++ cypress/integration/contact_page_spec.js
@@ -1,7 +1,15 @@
 describe('Contact Form', () => {
-  context('when page loads', () => {
-    it('sample test', () => {
-      cy.visit('http://sample-website.beforeyoubid.com.au/contact')
+  let url = 'http://sample-website.beforeyoubid.com.au/contact'
+
+  context('when none of the fields are filled', () => {
+    it('returns validation error messages', () => {
+      cy.visit(url)
+
+      cy.contains('Submit').click()
+
+      cy.contains('Your name is required')
+      cy.contains('Your email is required')
+      cy.contains('Did you forget to leave us a message?')
     })
   })
 })
