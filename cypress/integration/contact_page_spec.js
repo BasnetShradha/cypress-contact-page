eb06054df75b30b02b57e6fb181bfbe20542a8f3
BasnetShradha <basnetshradha10@gmail.com>
2020-10-19 20:37:33 +1100

    DRY specs.

--- cypress/integration/contact_page_spec.js
+++ cypress/integration/contact_page_spec.js
@@ -16,25 +16,27 @@ describe('Contact Form', () => {
 
   context('when some of the fields have various forms of data', () => {
     context('contact name', () => {
+      beforeEach(() => {
+        cy.visit(url)
+
+        cy.get('[data-cy=input-email]')
+          .click()
+          .type('bella.vista@test.com')
+
+        cy.get('[data-cy=input-phone]')
+          .click()
+          .type('1234456789')
+
+        cy.get('[data-cy=input-message]')
+          .click()
+          .type('Hi, This is Bella Vista.')
+      })
+
       context('when contact name has special characters', () => {
         it('does not return validation error message', () => {
-          cy.visit(url)
-
           cy.get('[data-cy=input-name]')
             .click()
             .type('Bella %%% Vista')
-
-          cy.get('[data-cy=input-email]')
-            .click()
-            .type('bella.vista@test.com')
-
-          cy.get('[data-cy=input-phone]')
-            .click()
-            .type('1234456789')
-
-          cy.get('[data-cy=input-message]')
-            .click()
-            .type('Hi, This is Bella Vista.')
 
           cy.contains('Submit').click()
           cy.contains('Your message has been sent :)')
@@ -43,22 +45,9 @@ describe('Contact Form', () => {
 
       context('when contact name are numbers', () => {
         it('does not return validation error message', () => {
-          cy.visit(url)
           cy.get('[data-cy=input-name]')
             .click()
             .type('123456')
-
-          cy.get('[data-cy=input-email]')
-            .click()
-            .type('bella.vista@test.com')
-
-          cy.get('[data-cy=input-phone]')
-            .click()
-            .type('1234456789')
-
-          cy.get('[data-cy=input-message]')
-            .click()
-            .type('Hi, This is Bella Vista.')
 
           cy.contains('Submit').click()
           cy.contains('Your message has been sent :)')
@@ -67,22 +56,9 @@ describe('Contact Form', () => {
 
       context('when contact name has long string', () => {
         it('does not return validation error message', () => {
-          cy.visit(url)
           cy.get('[data-cy=input-name]')
             .click()
             .type(long_string)
-
-          cy.get('[data-cy=input-email]')
-            .click()
-            .type('bella.vista@test.com')
-
-          cy.get('[data-cy=input-phone]')
-            .click()
-            .type('1234456789')
-
-          cy.get('[data-cy=input-message]')
-            .click()
-            .type('Hi, This is Bella Vista.')
 
           cy.contains('Submit').click()
           cy.contains('Your message has been sent :)')
